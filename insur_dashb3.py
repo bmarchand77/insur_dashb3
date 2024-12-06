9dcccb8452ad4f8e5d6158fb78438c1bc186cac5
bmarchand77 <133062311+bmarchand77@users.noreply.github.com>
2024-12-06 17:58:04 -0400

    Update insur_dashb3.py

--- insur_dashb3.py
+++ insur_dashb3.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load data
-data = pd.read_csv("C:\\Users\\bened\\test_data\\vehicle_insurance_claims.csv", parse_dates=["Policy_Start_Date", "Policy_End_Date", "Claim_Date", "Payment_Date"])
+data = pd.read_csv("vehicle_insurance_claims.csv", parse_dates=["Policy_Start_Date", "Policy_End_Date", "Claim_Date", "Payment_Date"])
 
 # Title for the dashboard layout sections
 st.write("Designed by Benedict Marchand")
